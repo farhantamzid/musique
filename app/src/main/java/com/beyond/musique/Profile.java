c71dc376e57f2a6a71710bbd5970b2bd97dfe7f4
farhantamzid <farhantamzid@gmail.com>
2024-12-26 13:45:31 +0600

    pretty much done with profile page without firestore integreation

--- app/src/main/java/com/beyond/musique/Profile.java
+++ app/src/main/java/com/beyond/musique/Profile.java
@@ -1,16 +1,27 @@
 package com.beyond.musique;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Profile extends Fragment {
+
+    TextView name;
+    TextView email;
+    Button signOutButton;
+
+    FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +41,27 @@ public class Profile extends Fragment {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        name = rootView.findViewById(R.id.name);
+        email = rootView.findViewById(R.id.email);
+        signOutButton = rootView.findViewById(R.id.signOutButton);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                if (getActivity() != null) {
+                    getActivity().finish();
+
+                }
+
+            }
         });
 
         return rootView;
